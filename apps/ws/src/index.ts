3670b73eb3ece7c913c9b536e4c905ff0af8b26f
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2025-10-16 18:25:30 +0545

    check w/r from ssh

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -2,116 +2,115 @@ import { WebSocketServer } from "ws";
 import * as dotenv from "dotenv";
 dotenv.config();
 import { pub, initializeRedis } from "./redisconfig";
-import { User  } from "./UserClass";
-import { Socket_Sending,Socket_Sending_type } from "./type";
+import { User } from "./UserClass";
+import { Socket_Sending, Socket_Sending_type } from "./type";
 import { sectionMap } from "./UserClass";
 import { JoinMessegeHandling } from "./JoinAuth";
-import {loader , Sections} from "./sections"
+import { loader, Sections } from "./sections"
 import { Join_the_section } from "./sections/src/JoinSection";
 import { StorageInit } from "./redis/Storage";
-export const SectionIdList:string[] =[];
-export const userIdMapping= new Map<string,User>();
+export const SectionIdList: string[] = [];
+export const userIdMapping = new Map<string, User>();
 // redis
-(async()=>{
-    await initializeRedis()
-    StorageInit();
-     ServerHandeling(); //Redis Stuff
-     Sections(); //Sections Deletion after the server is re initiated
-     loader();  //User Defination
+(async () => {
+	await initializeRedis()
+	StorageInit();
+	ServerHandeling(); //Redis Stuff
+	Sections(); //Sections Deletion after the server is re initiated
+	loader();  //User Defination
 })(); //Making the callback function call to the redis
 
-let isJoined = false ; 
- function ServerHandeling(){
-    const wss = new WebSocketServer({port:8080});
-    wss.on("connection",(socket:any)=>{
-        socket.on("message",async(message:string)=>{
+let isJoined = false;
+function ServerHandeling() {
+	const wss = new WebSocketServer({ port: 8080 });
+	wss.on("connection", (socket: any) => {
+		socket.on("message", async (message: string) => {
 
-                let socketSendingVariable: Socket_Sending = {
-                    payload:{
-                    type:"res",
-                    commands:"",
-                    },
-                    type:Socket_Sending_type.Initial_Call,
-                }
-            
-             const messegeJson:Socket_Sending  = JSON.parse(message);
-           
-            if(messegeJson.token && messegeJson.type==Socket_Sending_type.Initial_Call){
-                const data = await JoinMessegeHandling(messegeJson.token,socketSendingVariable,socket);
-                if(data.status){
-                    socketSendingVariable.type = Socket_Sending_type.Initial_Call;
-                    socketSendingVariable.msg="success"
-                    socket.user = data.addeduser
+			let socketSendingVariable: Socket_Sending = {
+				payload: {
+					type: "res",
+					commands: "",
+				},
+				type: Socket_Sending_type.Initial_Call,
+			}
 
-                }else{
-                    socketSendingVariable.type = Socket_Sending_type.Initial_Call; 
-                    socketSendingVariable.msg="fail"
-                }
-                console.log('sending the responce of the Auth to the user');
-                socket.send(JSON.stringify(socketSendingVariable));
-            }
-            else if(messegeJson.payload.type=="req"){
-               
-                    switch(messegeJson.type){
+			const messegeJson: Socket_Sending = JSON.parse(message);
 
-                        case Socket_Sending_type.Join_Section:
-                            // Make the redis call simoultanous
-                            //@ts-ignore
-                            if(!socket.user.id){
-                                console.log("you are not authenticated");
-                                // Send the Socket mEsseging the error trigger
-                            } 
-                            
-                            const sectionCreation = await Join_the_section(messegeJson.sectionid || '' ,socket);
-                            if(sectionCreation==true){
-                                socketSendingVariable.type = Socket_Sending_type.Join_Section; 
-                                 getStream(socket);
-                                socketSendingVariable.msg="success"
-                                socket.send(JSON.stringify(socketSendingVariable));
-                            }
-                            else{
-                                console.log("Getting the information from the Sections");
-                            }
-                            break;
-                        case Socket_Sending_type.Create_Stream:
-                            messegeJson.payload.commands = "addQueue"
-                            pub.publish(messegeJson.sectionid||'',JSON.stringify(messegeJson));
-                            break; 
-                        case Socket_Sending_type.Stream_Man:
-                            if(messegeJson.payload.commands=="GetState"){
-                                const userId = socket.user.id ; 
-                                
-                                pub.publish(messegeJson.sectionid|| "",JSON.stringify({...messegeJson,userId}));
-                            }
-                            // take the Stream man functiona and either upvote downvote or get the current state from the redis application
-                            break; 
-                        case Socket_Sending_type.Create_Section:
-                            console.log('You are trying to create the section');
-                            break;
-                            default :
-                            console.log("Error But here is your messege",messegeJson);
-                            return ; 
-                    }
-            }
-        
-    })
-    socket.on("close",(socket:any)=>{
-        console.log(`before:${sectionMap.size}`);
-        // remove the user From the array
-           if (socket.user && socket.user.id) {
-        userIdMapping.delete(socket.user.id);
-        for (const [sections, users] of sectionMap) {
-            sectionMap.set(sections, users.filter(u => u.id !== socket.user.id));
-        }
-    }
-        console.log(sectionMap.size);
-    })
+			if (messegeJson.token && messegeJson.type == Socket_Sending_type.Initial_Call) {
+				const data = await JoinMessegeHandling(messegeJson.token, socketSendingVariable, socket);
+				if (data.status) {
+					socketSendingVariable.type = Socket_Sending_type.Initial_Call;
+					socketSendingVariable.msg = "success"
+					socket.user = data.addeduser
 
-});
- }
+				} else {
+					socketSendingVariable.type = Socket_Sending_type.Initial_Call;
+					socketSendingVariable.msg = "fail"
+				}
+				console.log('sending the responce of the Auth to the user');
+				socket.send(JSON.stringify(socketSendingVariable));
+			}
+			else if (messegeJson.payload.type == "req") {
 
-const  getStream  = ( socket:WebSocket)=>{
-    // Fetch the data from the Redis Client
+				switch (messegeJson.type) {
 
- }
- 
+					case Socket_Sending_type.Join_Section:
+						// Make the redis call simoultanous
+						//@ts-ignore
+						if (!socket.user.id) {
+							console.log("you are not authenticated");
+							// Send the Socket mEsseging the error trigger
+						}
+
+						const sectionCreation = await Join_the_section(messegeJson.sectionid || '', socket);
+						if (sectionCreation == true) {
+							socketSendingVariable.type = Socket_Sending_type.Join_Section;
+							getStream(socket);
+							socketSendingVariable.msg = "success"
+							socket.send(JSON.stringify(socketSendingVariable));
+						}
+						else {
+							console.log("Getting the information from the Sections");
+						}
+						break;
+					case Socket_Sending_type.Create_Stream:
+						messegeJson.payload.commands = "addQueue"
+						pub.publish(messegeJson.sectionid || '', JSON.stringify(messegeJson));
+						break;
+					case Socket_Sending_type.Stream_Man:
+						if (messegeJson.payload.commands == "GetState") {
+							const userId = socket.user.id;
+
+							pub.publish(messegeJson.sectionid || "", JSON.stringify({ ...messegeJson, userId }));
+						}
+						// take the Stream man functiona and either upvote downvote or get the current state from the redis application
+						break;
+					case Socket_Sending_type.Create_Section:
+						console.log('You are trying to create the section');
+						break;
+					default:
+						console.log("Error But here is your messege", messegeJson);
+						return;
+				}
+			}
+
+		})
+		socket.on("close", (socket: any) => {
+			console.log(`before:${sectionMap.size}`);
+			// remove the user From the array
+			if (socket.user && socket.user.id) {
+				userIdMapping.delete(socket.user.id);
+				for (const [sections, users] of sectionMap) {
+					sectionMap.set(sections, users.filter(u => u.id !== socket.user.id));
+				}
+			}
+			console.log(sectionMap.size);
+		})
+
+	});
+}
+
+const getStream = (socket: WebSocket) => {
+	console.log("temp commit for the ssh verification");
+}
+
